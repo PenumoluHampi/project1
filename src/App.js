84ee917897175a9f6d0016334334d7e8a0cf76e8
PenumoluHampi <penumoluhampi@gmail.com>
2020-03-12 15:57:25 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Profile from './Profile';
+import Resume from './Resume';
+import {BrowserRouter,Route,Link} from 'react-router-dom'
+let App=()=>{
+  return (
+    <BrowserRouter>
+      <Route exact path='/' component={Profile}></Route>
+      <Route exact path="/resume" component={Resume} />
+    </BrowserRouter>
+    )
+}
+export default App;
